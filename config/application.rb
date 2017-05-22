c9d7654824335a223e664e019f6d9c38adf7978e
Ian Starker <istarker1@gmail.com>
2017-05-22 19:03:56 -0400

    added foundation and devise

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,13 @@ Bundler.require(*Rails.groups)
 module TableOne
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.load_defaults 5.1 - COMMENTED
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil - COMMENTED
   end
 end
