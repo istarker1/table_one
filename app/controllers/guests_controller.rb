25ee60c03b5e7ad0555f561e99bc08cfbc5a578d
Ian Starker <istarker1@gmail.com>
2017-05-30 20:22:19 -0400

    refactoring

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -13,31 +13,18 @@ class GuestsController < ApplicationController
     @event = Event.find(params[:event_id])
     @guest = Guest.new(guest_params)
     @guest.event_id = params[:event_id]
-    if @guest.guest_relationship.name == "Choose one or create your own"
-      @guest.relationship_id = create_custom_relationship(
-        params[:guest][:relationship] ,params[:event_id])
-    end
+    check_for_custom_relationship
     @plusone = Plusone.new(plusone_params)
     if @guest.valid?
       if @plusone.first_name == "" && @plusone.last_name == "" # no plusone data entered
-        @guest.save
-        flash[:notice] = "Guest added!"
-        redirect_to @event
+        valid_guest_no_plusone
       elsif @plusone.valid?
-        @guest.save
-        @plusone.guest = @guest
-        @plusone.save
-        redirect_to @event
+        valid_guest_valid_plusone
       else
-        @errors = @plusone.errors.full_messages
-        @relationships = @event.relationships
-        render action: 'new'
+        valid_guest_invalid_plusone
       end
     else
-      @errors = @guest.errors.full_messages
-      @relationships = @event.relationships
-      @plusone = Plusone.new
-      render action: 'new'
+      invalid_guest
     end
   end
 
@@ -53,13 +40,42 @@ class GuestsController < ApplicationController
     params.require(:plusone).permit(:first_name, :last_name, :notes)
   end
 
+  def check_for_custom_relationship
+    if @guest.guest_relationship.name == "Choose one or create your own"
+      @guest.relationship_id = create_custom_relationship(
+        params[:guest][:relationship] ,params[:event_id])
+    end
+  end
+
   def create_custom_relationship(rel_name, event)
     rel = Relationship.new(name: rel_name, event_id: event.to_i)
-    if rel.save
-      rel.id
-    else
-      nil
-    end
+    rel.id if rel.save
+  end
+
+  def valid_guest_no_plusone
+    @guest.save
+    flash[:notice] = "Guest added!"
+    redirect_to @event
+  end
+
+  def valid_guest_valid_plusone
+    @guest.save
+    @plusone.guest = @guest
+    @plusone.save
+    redirect_to @event
+  end
+
+  def valid_guest_invalid_plusone
+    @errors = @plusone.errors.full_messages
+    @relationships = @event.relationships
+    render action: 'new'
+  end
+
+  def invalid_guest
+    @errors = @guest.errors.full_messages
+    @relationships = @event.relationships
+    @plusone = Plusone.new
+    render action: 'new'
   end
 
 end
